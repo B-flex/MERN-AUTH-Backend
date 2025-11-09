dafd76678336aa99be6439ec8cd401268a107598
unknown <bflexoyobio@gmail.com>
2025-11-09 14:38:10 +0100

    initial commit for backend files

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import cookieParser from 'cookie-parser'
+
+import connectDB from './config/mongodb.js'
+import authRouter from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js'
+
+const app = express()
+const PORT = process.env.PORT || 4000
+connectDB()
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}));
+
+
+
+app.get('/', (req, res)=>{
+    res.send('home working')
+})
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`localhost ${PORT} is running`)
+})
---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const authRouter = express.Router();
+import {register, login, logout, sendVerifyOtp, verifyEmail, isAuthenticated, sendRestOtp, resetPassword} from '../controllers/authController.js'
+import userAuth from '../middleware/userAuth.js';
+
+
+
+authRouter.post('/register', register)
+authRouter.post('/login', login)
+authRouter.post('/logout', logout)
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp)
+authRouter.post('/verify-account', userAuth, verifyEmail)
+authRouter.post('/is-auth', userAuth, isAuthenticated)
+authRouter.post('/send-reset-otp', sendRestOtp)
+authRouter.post('/reset-password', resetPassword)
+
+export default authRouter;
